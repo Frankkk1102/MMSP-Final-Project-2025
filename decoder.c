a0ad23f0c871587799d58d15a2154279db121dde
Frankkk1102 <1102frank@gmail.com>
2025-12-27 16:48:32 +0800

    Update decoder.c

--- decoder.c
+++ decoder.c
@@ -96,6 +96,9 @@ static void bmp_write_24(const char *path, const ImageRGB *im){
     ih.biBitCount=24;
     ih.biCompression=0;
     ih.biSizeImage=0;
+    ih.biXPelsPerMeter = 2835;
+    ih.biYPelsPerMeter = 2835;
+
 
     fwrite(&fh,sizeof(fh),1,fp);
     fwrite(&ih,sizeof(ih),1,fp);
